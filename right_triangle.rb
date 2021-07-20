846780fa42897b1177583f191f87f80104cda453
BelovVladimirO <BelovVladimirO@yandex.ru>
2021-07-20 14:35:14 +0300

    Исправления

--- right_triangle.rb
+++ right_triangle.rb
@@ -7,11 +7,11 @@ puts "Сторона c треугольника?"
 sides << gets.chomp.to_i
 sides.sort!
 if sides[0]**2 + sides[1]**2 == sides[2]**2
-    puts "Треугольник прямоугольный"
+  puts "Треугольник прямоугольный"
 end
 if sides[0] == sides[1]  && sides[1] == sides[2]
-    puts "Треугольник равнобедренный"
+  puts "Треугольник равнобедренный"
 elsif sides[0] == sides[1] || sides[0] == sides[2] || sides[1] == sides[2]
-    puts "Треугольник равносторонний"
+  puts "Треугольник равносторонний"
 end  
     
--- the_quadratic_equation.rb
+++ the_quadratic_equation.rb
@@ -6,10 +6,10 @@ puts "Введите коэффициент c"
 c = gets.chomp.to_i
 d = b**2 - 4 * (a * c)
 if d > 0
-    puts "#{d}, #{x1 = (-b + Math.sqrt(d))/(2 * a)}" 
-    puts "#{d}, #{x1 = (-b - Math.sqrt(d))/(2 * a)}"
+  puts "#{d}, #{x1 = (-b + Math.sqrt(d))/(2 * a)}" 
+  puts "#{d}, #{x1 = (-b - Math.sqrt(d))/(2 * a)}"
 elsif d == 0 
-    puts "#{d}, #{x1 = (-b + Math.sqrt(d))/(2 * a)}" 
+  puts "#{d}, #{x1 = (-b + Math.sqrt(d))/(2 * a)}" 
 else d < 0 
-    puts "#{d},Корней нет"
+  puts "#{d},Корней нет"
 end
