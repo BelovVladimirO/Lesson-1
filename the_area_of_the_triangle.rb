4f0533df93a268bce9e45c9d2f05e8d8f391e84e
BelovVladimirO <BelovVladimirO@yandex.ru>
2021-07-23 16:43:14 +0300

    1

--- the_area_of_the_triangle.rb
+++
@@ -1,6 +0,0 @@
-puts "Какое основание у треугольника?"
-a = gets.chomp.to_i
-puts "Какая высота у треугольника?"
-h = gets.chomp.to_i
-square = (a * h) / 2
-puts "Площадь треугольника равна #{square.to_i}"
