ae1707567a6101da4ef759bf16b86c87c56b2363
BelovVladimirO <BelovVladimirO@yandex.ru>
2021-07-23 11:41:45 +0300

    Последние исправления

--- lesson_2/task5.rb
+++ lesson_2/task5.rb
@@ -1,29 +1,18 @@
 months_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 result = 0
 
- #year = (1000..2021).to_a
-#year.each do |y|
-#if ((y % 400 == 0) && (y % 100 == 0))
-#  result   += 1
-#elsif (y % 4 == 0)
-#    result  += 1
-#end
-#end
-
-# ПРОВЕРИЛ 1021 ГОД КАКИЕ ВИСОКОСНЫЕ, А КАКИЕ НЕТ, НО НЕ СМОГ ПРИМЕНИТЬ.
-
+puts "Введите год"
+year = gets.chomp.to_i
+if ((year % 400 == 0) && (year % 100 == 0))
+  months_days[1] = 29 
+elsif (year % 4 == 0)
+  months_days[1] = 29
+end
 puts "Введите дату"
 date = gets.chomp.to_i
 result += date
 puts "Введите месяц"
 month = gets.chomp.to_i
-day = (months_days.take(month).sum - result)
-puts "Введите год"
-year = gets.chomp.to_i
+day = (months_days.take(month - 1).sum) + result 
 
-if ((year % 400 == 0) && (year % 100 == 0))
-  result += 1
-elsif (year % 4 == 0)
-  result += 1
-end
-p "Порядковый номер в годе #{day}" 
+p "Порядковый номер в годе #{day}"
